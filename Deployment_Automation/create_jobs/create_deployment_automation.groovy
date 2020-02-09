992ac3ce8ca2bc049e0cf4f392625297bc3fa82a
Anthony Magner <anthony.magner@gmail.com>
2020-02-08 19:00:19 -0700

    asfd

--- Deployment_Automation/create_jobs/create_deployment_automation.groovy
+++ Deployment_Automation/create_jobs/create_deployment_automation.groovy
@@ -1,18 +1,25 @@
 #!/usr/bin/env groovy
 import groovy.json.JsonSlurper
 
+env.WORKSPACE = pwd()
+
+
 String currentDir = new File(".").getAbsolutePath()
+String cfg_path = "${env.WORKSPACE}/"
 
-print ("Current dir is ${currentDir}")
 
-def inputFile = new File('.//MyConfig.json')
-def inputJSON = new JsonSlurper().parse(inputFile)
-def keys = inputJSON.keySet() as List
+println ("Current dir is ${currentDir}")
+println ("Config Path is ${cfg_path}")
+
+//def inputFile = new File('.//MyConfig.json')
+//def inputJSON = new JsonSlurper().parse(inputFile)
+//def keys = inputJSON.keySet() as List
 
 def project = 'antman25/PipelineTesting'
 def branchApi = new URL("https://api.github.com/repos/${project}/branches")
 def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
 
+jsonParse(readFile('Deployment_Automation/config/MyConfig.json')) 
 
 
 inputJson.each { k, v ->
@@ -28,7 +35,7 @@ folder('Deployment Automation') {
 branches.each {
     def branchName = it.name
     def jobName = "${branchName}".replaceAll('/','-')
-  	def jobPath = "Deployment Automation/" + jobName
+    def jobPath = "Deployment Automation/" + jobName
     job(jobPath) {
         scm {
             git("git://github.com/${project}.git", branchName)
@@ -37,4 +44,9 @@ branches.each {
             print("test")
         }
     }
+}
+
+@NonCPS
+def jsonParse(text) {
+        return new groovy.json.JsonSlurperClassic().parseText(text);
 }
