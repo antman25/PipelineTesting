f56adb0beff95833b62f176a8ecbb7340a9af9d2
Anthony Magner <anthony.magner@gmail.com>
2020-04-26 19:22:37 -0600

    test

--- Deployment_Automation/utilities/print_args.py
+++ Deployment_Automation/utilities/print_args.py
@@ -4,7 +4,11 @@ import sys
 
 print ("------------------------")
 
+output_file = open('/tmp/test', 'w')
+
 for arg in sys.argv:
 	print ("ARG %s" % arg)
+	output_file.write(arg + "\n")
+output_file.close()
 
 print ("------------------------")
