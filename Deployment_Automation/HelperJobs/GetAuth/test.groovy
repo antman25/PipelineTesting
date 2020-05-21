c21ac3036ac18cbfe8ac2ee7377dfddbce5a73b0
Anthony Magner <anthony.magner@gmail.com>
2020-05-20 23:54:26 -0600

    updates

---
+++ Deployment_Automation/HelperJobs/GetAuth/test.groovy
@@ -0,0 +1,18 @@
+import hudson.model.Hudson
+import hudson.security.GlobalMatrixAuthorizationStrategy
+import hudson.security.ProjectMatrixAuthorizationStrategy
+
+def authStrategy = Hudson.instance.getAuthorizationStrategy()
+
+//print (authStrategy)
+
+
+if(authStrategy instanceof hudson.security.GlobalMatrixAuthorizationStrategy)
+{
+            
+   def permissions = authStrategy.getGrantedPermissions()
+   //print (permissions)
+	permissions.each { key, val ->
+		print("KEY: ${key} -- VAL: ${val}"
+}
+
