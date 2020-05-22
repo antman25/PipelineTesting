8c2e7a288d1ac63924cc4b081b0e1dfa8c7b1602
Anthony Magner <anthony.magner@gmail.com>
2020-05-21 21:13:17 -0600

    asdf

--- Deployment_Automation/HelperJobs/GetAuth/test.groovy
+++ Deployment_Automation/HelperJobs/GetAuth/test.groovy
@@ -2,17 +2,23 @@ import hudson.model.Hudson
 import hudson.security.GlobalMatrixAuthorizationStrategy
 import hudson.security.ProjectMatrixAuthorizationStrategy
 
-def authStrategy = Hudson.instance.getAuthorizationStrategy()
-
-//print (authStrategy)
 
 
-if(authStrategy instanceof hudson.security.GlobalMatrixAuthorizationStrategy)
+def getAuth()
 {
-            
-   def permissions = authStrategy.getGrantedPermissions()
-   //print (permissions)
-	permissions.each { key, val ->
+	def authStrategy = Hudson.instance.getAuthorizationStrategy()
+
+	//print (authStrategy)
+
+	
+	if(authStrategy instanceof hudson.security.GlobalMatrixAuthorizationStrategy)
+	{
+        	def permissions = authStrategy.getGrantedPermissions()
+	   //print (permissions)
+		permissions.each { key, val ->
 		print("KEY: ${key} -- VAL: ${val}"
+	}
 }
+
+return this
 
